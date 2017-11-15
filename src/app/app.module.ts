f20c9629dfd9d9e6449d78c145edef90139ccdde
Brendon Roberto <Brendon.Roberto@onbase.com>
2017-11-15 16:37:50 -0500

    refactor - break-damage-form: Split component
    * Create break-damage-form.component
    * Move implementation of break damage to break-damage-form
    * Fix up styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from '@components/app/app.component';
+import { BreakDamageFormComponent } from '@components/break-damage-form/break-damage-form.component';
 
 import { AppService } from '@services/app.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreakDamageFormComponent
   ],
   imports: [
     BrowserModule,
